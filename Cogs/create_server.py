bb18c4fa92e0faf3818054159fd4fa38ac579882
mathieu <degueurce.mathieu@gmail.com>
2023-11-27 11:07:36 +0100

    envoie d'erreur pour affichage front tentative fix bug redirect avant fin
    Took 36 minutes

--- Cogs/create_server.py
+++ Cogs/create_server.py
@@ -12,11 +12,17 @@ def create_server_cogs(database, alert, dir_path):
         return render_template('create_server.html', alert=alert)
     elif request.method == 'POST':
         if not request.form['server-name'] or not request.form['server-cmd']:
-            return redirect(url_for('create_server', alert=True))
+            return redirect(url_for('create_server', alert='field-empy'))
 
-        mkdir(dir_path + secure_filename(request.form['server-name']))
-        database.insert("""INSERT INTO cantina_server_manager.server(owner_token, name, server_token, 
-        server_run_command, server_path, group_acces) VALUES (%s, %s, %s, %s, %s, %s)""",
+        try:
+            mkdir(dir_path + secure_filename(request.form['server-name']))
+            database.insert("""INSERT INTO cantina_server_manager.server(owner_token, name, server_token, 
+            server_run_command, server_path, group_acces) VALUES (%s, %s, %s, %s, %s, %s)""",
                         (request.cookies.get("token"), request.form['server-name'], "0", request.form['server-cmd'],
                          dir_path + secure_filename(request.form['server-name']), 0))
-        return redirect(url_for('server'))
+            return redirect(url_for('server'))
+        except Exception as e:
+            if e == FileExistsError:
+                return redirect(url_for('create_server', alert='file-existe'))
+            elif e[0] == 1054:
+                return redirect(url_for('create_server', alert='db-error'))
